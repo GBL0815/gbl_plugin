ff9a1407c92bf8514e2763663c9fb8eedadcc16a
beiliang.ge <32394350+GBL0815@users.noreply.github.com>
2021-04-07 09:53:40 +0800

    失败了

---
+++ build/build.ts
@@ -0,0 +1,27 @@
+const fs = require('fs-extra')
+const path = require('path')
+
+const isDir = (dir: unknown) => {
+  return fs.lstatSync(dir).isDirectory()
+}
+
+const transformStr3 = (str: string) => {
+  const re = /-(\w)/g
+  return str.replace(re, ($0, $1) => {
+    return $1.toUpperCase()
+  })
+}
+
+const json = {}
+const dir = path.join(__dirname, '../packages')
+const files = fs.readdirSync(dir)
+files.forEach(file => {
+  const absolutePath = path.join(dir, file)
+  if (isDir(absolutePath)) {
+    const fileKey = transformStr3(file.replace('orange_', ''))
+    // console.log(fileKey)
+    json[fileKey] = `/packages/${file}/index.ts`
+  }
+})
+
+// console.log(JSON.stringify(json))
